afe860214482b7ba5f465ea93be46b41ebda1b86
Michael Hajný <47132583+3n0wd3n@users.noreply.github.com>
2022-02-10 11:27:50 +0100

    Add files via upload

---
+++ readme.md
@@ -0,0 +1,6 @@
+<!-- 
+BEFORE STARTED YOU MUST PUT THIS ON TERMINAL
+npm i express mongoose ejs method-override
+npm i --save-dev nodemo
+npm run devStart 
+-->
---
+++ server.js
@@ -0,0 +1,35 @@
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const express = require('express')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+const Article = require('./models/article')
+const articleRouter = require('./routes/articles')
+const app = express()
+const port = process.env.PORT || 5000
+
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+})
+
+const db = mongoose.connection
+
+db.on('error', error => 
+  console.error(error))
+db.once('open', () => 
+  console.log('Conected to mangoose'))
+
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
+
+app.get('/', async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
+
+app.use('/articles', articleRouter)
+
+app.listen(port)
